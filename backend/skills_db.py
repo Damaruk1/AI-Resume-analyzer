2a4ea64b6248e6a7d90b555eedf140859f416c7d
Damaruk <damarukabhishek@gmail.com>
2026-03-09 14:33:54 +0530

    Add skills and action verbs to skills_db.py

---
+++ backend/skills_db.py
@@ -0,0 +1,12 @@
+SKILLS_DB = [
+"python","java","c++","sql","machine learning","deep learning",
+"pandas","numpy","tensorflow","pytorch",
+"react","node","javascript","typescript",
+"docker","kubernetes","aws","azure","gcp",
+"git","linux","flask","fastapi"
+]
+
+ACTION_VERBS = [
+"developed","built","implemented","optimized","designed",
+"led","managed","created","improved","engineered"
+]
